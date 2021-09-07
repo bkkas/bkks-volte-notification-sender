3b2c3066346139c7351c8107035d189370272655
rashmi-bkk <rashmi.kumari@bkk.no>
2021-09-07 17:22:31 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='calculator',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:bkkas/bkks-volte-package-creation-test.git',
+    author='Rashmi Kumari',
+    author_email='rashmi.kumari@bkk.no',
+    license='unlicense',
+    packages=['calculator'],
+    zip_safe=False
+)
