747107afda55b39399ac75a6c9428083f1bb8627
rashmi-bkk <rashmi.kumari@bkk.no>
2021-09-07 16:56:28 +0200

    Added basic setup

---
+++ calculator/area.py
@@ -0,0 +1,16 @@
+# function definition 1
+def circle(r):
+    print("Area of circle:",3.14*r*r)
+    return
+# function definition 2
+def square(l):
+    print("Area of square:",l*l)
+    return
+# function definition 3
+def rectangle(l,b):
+    print("Area of rectangle:",l*b)
+    return
+# function definition 4
+def triangle(b,h):
+    print("Area of triangle:",0.5*b*h)
+    return
