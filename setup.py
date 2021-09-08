4ab3934dd5da4c3bc67923f07aa4ceff8f3b692e
rashmi-bkk <rashmi.kumari@bkk.no>
2021-09-08 16:06:33 +0200

    Updated package name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='bkks-volte-notification-sender',
+    name='bkks_volte_notification_sender',
     version='0.1',
     description='My private package from private github repo',
     url='git@github.com:bkkas/bkks-volte-notification-sender.git',
     author='Rashmi Kumari,Jesus Gazol',
     author_email='rashmi.kumari@bkk.no',
     license='Volte',
-    packages=['bkks-volte-notification-sender'],
+    packages=['bkks_volte_notification_sender'],
     install_requires=[
           'azure-servicebus',
     ],
