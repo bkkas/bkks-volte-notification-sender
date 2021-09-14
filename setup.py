9083d5694d7ef3c0455192d8ce459242c1acda1c
rashmi-bkk <rashmi.kumari@bkk.no>
2021-09-14 10:32:21 +0200

    Updated url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.0',
     description='A simple package to send messages to azure service bus',
     license = "LGPL3",
-    url='https://github.com/bkkas/bkks-volte-notification-sender.git',
+    url='git@github.com:bkkas/bkks-volte-notification-sender.git',
     author='bkks-volte',
     author_email='rashmi.kumari@bkk.no,',
     packages=['bkks_volte_notification_sender'],
