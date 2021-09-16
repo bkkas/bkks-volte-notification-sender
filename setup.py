3d6af30fad4810684712053bc0086d128e246f93
Rashmi Kumari <Rashmi.kumari@bkk.no>
2021-09-16 14:36:21 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bkks_volte_notification_sender",
-    version="0.2.0",
+    version="0.3.0",
     description="A simple package to send messages to a queue azure service bus",
     license="LGPL3",
     url="git@github.com:bkkas/bkks-volte-notification-sender.git",
