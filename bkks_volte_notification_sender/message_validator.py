c424ccfbc34168a9a0207bb51f071fe541b97d7c
rashmi-bkk <rashmi.kumari@bkk.no>
2021-09-17 13:33:22 +0200

    precommit test

--- bkks_volte_notification_sender/message_validator.py
+++ bkks_volte_notification_sender/message_validator.py
@@ -1,6 +1,10 @@
 from email_validator import validate_email, EmailNotValidError
 from bkks_volte_notification_sender.notification_details import (
     NotificationType,
+    
+    
+    
+    
     NotificationDetails,
 )
 
